2e7865401a7b324645731dc10e883e445435a9bf
Khairul Alam <khairul431743@gmail.com>
2024-01-11 19:18:41 +0600

    added react hook form in registration page

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,6 +1,11 @@
+import { useForm } from "react-hook-form";
 
 
 const Registration = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -10,30 +15,51 @@ const Registration = () => {
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     </div>
                     <div className="card shrink-0 w-full w-1/2 shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Name" className="input input-bordered" required />
+                                <input type="text" {...register("name", { required: true })} placeholder="name" name="name" className="input input-bordered" />
+                                {errors.name && <span className="text-red-600">Name field is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="text" placeholder="Photo URL" className="input input-bordered" required />
+                                <input type="text" {...register("photURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" {...register("email", { required: true })} placeholder="email" name="email" className="input input-bordered" />
+                                {errors.email && <span className="text-red-600">Email field is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]/,
+                                    maxLength: 20
+                                })}
+                                    name="password" placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && (
+                                    <p className="text-red-600">Password is required</p>
+                                )}
+                                {errors.password?.type === 'minLength' && (
+                                    <p className="text-red-600">Password must be 6 characters</p>
+                                )}
+                                {errors.password?.type === 'maxLength' && (
+                                    <p className="text-red-600">Password must be less than 20 characters</p>
+                                )}
+                                {errors.password?.type === 'pattern' && (
+                                    <p className="text-red-600">Password must have one digit, one uppercase, one lowercase and one special character </p>
+                                )}
                                 
                             </div>
                             <div className="form-control mt-6">
