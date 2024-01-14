79b74c2e6ea658c085066043085e01785565bab6
Khairul Alam <khairul431743@gmail.com>
2024-01-14 16:56:23 +0600

    added banner photo, title and description

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -37,11 +37,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/Y4sHWRj/gogoodguru-com-workout-post-header-cardio-blast.jpg" className="w-1/2 rounded-lg shadow-2xl" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Cardio Blast!</h1>
+                                <p className="py-6">Ignite your metabolism and boost cardiovascular endurance with our high-energy Cardio Blast classes. Get ready to sweat, burn calories, and achieve your fitness goals.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -49,11 +49,11 @@ const Banner = () => {
                 <SwiperSlide>
                     <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/Hqn2Qzy/Flow-Chi-Fusion-Untitled-Page-1-1024x374.png" className="w-1/2 rounded-lg shadow-2xl" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Strength Fusion!</h1>
+                                <p className="py-6">Combine the power of strength training with dynamic movements in our Strength Fusion classes. Sculpt and tone your muscles for a stronger, leaner you.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -61,11 +61,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/jbBPfJz/informative-banner-energy-harmony-yoga-flat-82574-11632.jpg" className="w-1/2 rounded-lg shadow-2xl" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Yoga Harmony!</h1>
+                                <p className="py-6">Find balance and inner peace in our Yoga Harmony classes. Whether you are a beginner or an experienced yogi, join us to improve flexibility, reduce stress, and enhance overall well-being.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -73,11 +73,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/wSSfr9D/featured-image-workout-772x516.png" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">HIIT Revolution!</h1>
+                                <p className="py-6">Experience the intensity of High-Intensity Interval Training (HIIT) with our HIIT Revolution classes. Torch calories, improve endurance, and push your limits.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -85,11 +85,12 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/F7zbHZq/flat-hand-drawn-dance-fitness-class-illustration-23-2148867249.jpg" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Dance Fit Fiesta!</h1>
+                                <p className="py-6"> Dance your way to fitness in our Dance Fit Fiesta classes. Groove to the beat, have fun, and burn calories with our energetic dance routines.
+Button: Explore Classes</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -97,11 +98,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/t49W7JR/assets-tmp-catalogue-banner-1480-840-px-69-1703284396.png" className="w-1/2 rounded-lg shadow-2xl" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Core Sculpt!</h1>
+                                <p className="py-6">Strengthen and define your core muscles with our Core Sculpt classes. Targeted exercises for a strong, stable core and improved posture.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -109,11 +110,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/PDxzSHd/e2552b9195b4eb1c858539d75d81eb04.jpg" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Mindful Meditation!</h1>
+                                <p className="py-6">Achieve mental clarity and relaxation in our Mindful Meditation classes. Unwind, reduce stress, and cultivate mindfulness for a healthier mind and body.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -121,11 +122,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
-                            <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                            <img className='w-1/2 h-full' src="https://i.ibb.co/7bhVQWL/image.png" />
+                            <div className='w-1/2'>
+                                <h1 className="text-5xl font-bold">Spin Revolution!</h1>
+                                <p className="py-6">Join the Spin Revolution for an exhilarating indoor cycling experience. Burn calories, boost stamina, and enjoy the ride towards better fitness.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
@@ -133,11 +134,11 @@ const Banner = () => {
                 <SwiperSlide>
                 <div className="hero min-h-screen bg-base-200">
                         <div className="hero-content flex-col lg:flex-row">
-                            <img src="https://i.ibb.co/FbVrq41/8e88a8b711d6aea58af1c7268ef25b8f5c8b0f84-1140x620.png" className="w-1/2 rounded-lg shadow-2xl" />
+                            <img src="https://i.ibb.co/C05gCrs/small-total-body-bootcamp-cover.jpg" />
                             <div>
-                                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
+                                <h1 className="text-5xl font-bold">Total Body Bootcamp!</h1>
+                                <p className="py-6">Challenge yourself with our Total Body Bootcamp classes. A full-body workout designed to improve strength, endurance, and overall fitness.</p>
+                                <button className="btn btn-primary">Explore Classes</button>
                             </div>
                         </div>
                     </div>
